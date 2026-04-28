4220024942d4ff81a72ffc7b785388182bf05fca
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-04-28 22:54:58 +0300

    feat: toggle follow state

--- App.jsx
+++ App.jsx
@@ -1,7 +1,8 @@
 import Profile from "./PROFILE/PROFILE";
+import FollowButton from "./PROFILE/Button";
 
 function App() {
-    return <div> <Profile name="John Doe" role="Developer" /> </div>;
+    return <div> <Profile name="John Doe" role="Developer" /> <FollowButton />  </div>;
 }
 
 export default App;
